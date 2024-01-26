61a03bc0914b969652fc7b6fff2762029456df03
Sukhpreet Singh <peadevp@gmail.com>
2024-01-27 01:28:25 +0530

    basic drizzle setup

---
+++ src/lib/server/db/index.ts
@@ -0,0 +1,5 @@
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
+
+const client = createClient({ url: 'DATABASE_URL', authToken: 'DATABASE_AUTH_TOKEN' });
+export const db = drizzle(client);
